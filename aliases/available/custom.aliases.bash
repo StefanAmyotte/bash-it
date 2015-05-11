b192c02b40c925ed2c9c41bbc86c394492a894be
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-05-11 05:03:18 -0400

    moved all custom to the custom folder

--- aliases/available/custom.aliases.bash
+++
@@ -1,9 +0,0 @@
-alias fucking=sudo
-alias gti=git
-alias c2='ssh phirelight@c2'
-alias analytics='ssh phirelight@analytics'
-alias portal='ssh phirelight@portal'
-
-export PATH=$PATH:/home/stefan/bin/scala/bin
-export PATH=$PATH:/opt/bro/bin
-
---
+++ custom/aliases.bash
@@ -0,0 +1,25 @@
+# bin paths
+export PATH=$PATH:/home/stefan/bin/scala/bin
+export PATH=$PATH:/opt/bro/bin
+
+
+alias fucking=sudo
+alias gti=git
+alias c2='ssh phirelight@c2'
+alias analytics='ssh phirelight@analytics'
+alias portal='ssh phirelight@portal'
+alias h='history | grep $1'
+
+
+# common mispelled commands
+alias xs='cd'
+alias vf='cd'
+alias got='git '
+alias get='git '
+alias claer="clear"
+alias clera="clear"
+alias celar="clear"
+alias findgrep='grepfind'
+alias mann='man'
+alias updtae='update'
+alias vmi='vim'
