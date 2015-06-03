daf7d67e6b5bb069b4cd1bb2f0bd4130ce9920f6
Stefan Amyotte <stefan.amyotte@gmail.com>
2015-06-03 07:02:32 -0400

    tweak to custom

--- aliases/available/custom.aliases.bash
+++ aliases/available/custom.aliases.bash
@@ -1,21 +1,21 @@
 # bin paths
 export PATH=$PATH:/home/stefan/bin/scala/bin
-export PATH=$PATH:/opt/bro/bin
-
+export PATH=$PATH:/home/stefan/bin/bro/bin
+export PATH=$PATH:/home/stefan/bin/node/bin
+export PATH=$PATH:/home/stefan/bin/phantomjs/bin
 
 alias fucking=sudo
-alias gti=git
 alias c2='ssh phirelight@c2'
 alias analytics='ssh phirelight@analytics'
 alias portal='ssh phirelight@portal'
 alias h='history | grep $1'
 
-
 # common mispelled commands
 alias xs='cd'
 alias vf='cd'
-alias got='git '
-alias get='git '
+alias got='git'
+alias get='git'
+alias gti='git'
 alias claer="clear"
 alias clera="clear"
 alias celar="clear"
@@ -26,3 +26,28 @@ alias vmi='vim'
 
 # git
 alias gh='git hist'
+
+# enable color support of ls and also add handy aliases
+if [ -x /usr/bin/dircolors ]; then
+    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+    alias ls='ls --color=auto --group-directories-first'
+    alias dir='dir --color=auto'
+    alias vdir='vdir --color=auto'
+    alias grep='grep --color=auto'
+    alias fgrep='fgrep --color=auto'
+    alias egrep='egrep --color=auto'
+fi
+
+# advanced ls functions
+alias l="ls -1"
+alias la='ls -A'
+alias ll='ls -haltr'
+alias lx='ls -lXB'         # sort by extension
+alias lk='ls -lSr'         # sort by size, biggest last
+alias lc='ls -ltcr'        # sort by and show change time, most recent last
+alias lu='ls -ltur'        # sort by and show access time, most recent last
+alias lt='ls -ltr'         # sort by date, most recent last
+alias lm='ls -al | more'   # pipe through 'more'
+alias lr='ls -lR'          # recursive ls
+alias tree='tree -Csu'     # nice alternative to 'recursive ls'
+
