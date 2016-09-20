ce26850033477af691e51ecacf5343a1ed3754d6
Stefan Amyotte <stefan.amyotte@gmail.com>
2016-09-20 07:50:21 -0400

    tmux alise added to custom

--- aliases/available/custom.aliases.bash
+++ aliases/available/custom.aliases.bash
@@ -19,6 +19,9 @@ alias vmi='vim'
 alias gh='git hist'
 alias gs='git status'
 
+# tmux
+alias tmux='tmux -2'
+
 # enable color support of ls and also add handy aliases
 if [ -x /usr/bin/dircolors ]; then
     test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
